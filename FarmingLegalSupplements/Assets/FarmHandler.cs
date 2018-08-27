d1fdf6e2b6b94e14555f322e40d841605f09796a
MDS\benjamin.poi6329 <the343guy@hotmail.com>
2018-08-27 12:31:11 +1200

    Basic descriptors
    For on hover to update a pop-up tooltip

---
+++ FarmingLegalSupplements/Assets/FarmHandler.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FarmHandler : MonoBehaviour
+{
+    public string m_sDescription;
+    public int m_iResourceCount;
+	// Use this for initialization
+	void Start ()
+    {
+        m_iResourceCount = gameObject.GetComponentInChildren<farmController>().Yield;
+
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if (m_iResourceCount != gameObject.GetComponentInChildren<farmController>().Yield)
+        {
+            m_iResourceCount = gameObject.GetComponentInChildren<farmController>().Yield;
+        }
+	}
+}
