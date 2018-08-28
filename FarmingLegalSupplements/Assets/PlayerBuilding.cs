596724bd596108f9393251df3f7133db7f5db495
MDS\nicholas.gre6280 <nik.green@xtra.co.nz>
2018-08-28 12:09:21 +1200

    added the game ends when house is destoryed.

--- FarmingLegalSupplements/Assets/PlayerBuilding.cs
+++ FarmingLegalSupplements/Assets/PlayerBuilding.cs
@@ -1,8 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerBuilding : MonoBehaviour {
+    public enum type {green,white,home,shop,Def}
+    public type WhatAmI;
     public float BuildingHealth;
 	// Use this for initialization
 	void Start () {
@@ -11,9 +14,17 @@ public class PlayerBuilding : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+       
         if (BuildingHealth <= 0)
         {
-            Destroy(gameObject);
+            if (WhatAmI == type.home)
+            {
+                SceneManager.LoadScene(3);
+            }
+            else
+            {
+                Destroy(gameObject);
+            }
         }
 	}
 }
