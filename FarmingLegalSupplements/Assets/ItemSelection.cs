7d08c11322d154ad76ad4869df598fe60eadead5
BlairCorban <blaircorban@hotmail.com>
2018-10-02 14:55:00 +1300

    fixed main menu and commented item selection

--- FarmingLegalSupplements/Assets/ItemSelection.cs
+++ FarmingLegalSupplements/Assets/ItemSelection.cs
@@ -8,47 +8,57 @@ public class ItemSelection : MonoBehaviour {
     public GameObject SelectedObject;
     public Text name;
     public Text description;
-    // Use this for initialization
-    void Start () {
-		
-	}
 	
 	// Update is called once per frame
 	void Update () {
+        //if left mouse button pressed
+        
         if (Input.GetKeyDown(KeyCode.Mouse0))
         {
+            //create raycast to mouse position
             Ray ray = camera.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit;
-            if(Physics.Raycast(ray, out hit, 1000))
+            if (Physics.Raycast(ray, out hit, 1000))
             {
+                //if ray cast hits object with selectableobject script
                 if (hit.transform.GetComponent<SelectableObject>() != null)
                 {
+                    //object is selected
                     SelectedObject = hit.transform.gameObject;
-                   
-                    
                 }
+                //if raycast doesnt hit object with selectableobject script
                 else
                 {
+                    //unselect any objects that are set
                     SelectedObject = null;
                 }
             }
 
         }
+        
+           
+        
+        
 
-
+        //if an object is selected
         if (SelectedObject != null)
         {
+            //change ui text to objects specific text
             name.text = SelectedObject.GetComponent<SelectableObject>().Name;
             description.text = SelectedObject.GetComponent<SelectableObject>().Description;
-            if(SelectedObject.GetComponent<SelectableObject>().hasSpecificWindow)
-            {
-                //do stuff
-            }
         }
+        //if no object is selected
         else
         {
+            //set ui text to nothing
             name.text = "";
             description.text = "";
+            
         }
+
+
+
+
     }
+    
 }
