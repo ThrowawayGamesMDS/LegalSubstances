53a57afe339b580a4fb73e7bdaea1ebcd53e9f89
MDS\nicholas.gre6280 <nik.green@xtra.co.nz>
2018-10-02 14:07:34 +1300

    sound menu A2

---
+++ FarmingLegalSupplements/Assets/Scripts/menus.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class menus : MonoBehaviour {
+    public Slider Vol,Mus,SF;
+    public AudioMixer mix;
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+    public void mainVolChange(){
+        mix.SetFloat("master",(Vol.value));
+       
+    }
+    public void musicVolChange()
+    {
+        mix.SetFloat("music", Mus.value);
+    }
+    public void SFVolChange()
+    {
+        mix.SetFloat("sf", SF.value);
+    }
+}
