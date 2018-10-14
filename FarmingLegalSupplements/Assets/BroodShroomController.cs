dd12f771ef5e926f20726de83e9cde33311194f1
BlairCorban <blaircorban@hotmail.com>
2018-10-15 09:05:04 +1300

    start of broodshroom

---
+++ FarmingLegalSupplements/Assets/BroodShroomController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class BroodShroomController : MonoBehaviour {
+    public NavMeshAgent agent;
+    public GameObject Fiend;
+    public Animator anim;
+	// Use this for initialization
+	void Start () {
+        agent.SetDestination(GameObject.FindGameObjectWithTag("HomeBuilding").transform.position);
+        InvokeRepeating("repeat", 5, 10);
+	}
+	
+    void repeat()
+    {
+        StartCoroutine(Spawn());
+    }
+
+    IEnumerator Spawn()
+    {
+        anim.Play("spawning");
+        yield return new WaitForSeconds(2);
+        Instantiate(Fiend, transform.position, transform.rotation);
+    }
+
+	// Update is called once per frame
+	void Update () {
+	}
+
+
+}
