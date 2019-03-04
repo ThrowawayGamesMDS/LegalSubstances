4cfa4dbfbd46cfc67b5e36ce9b1963711871425b
MDS\benjamin.poi6329 <the343guy@hotmail.com>
2019-03-04 15:01:49 +1300

    Resource Spawning by Density
    Basic spawning by density implemented, bit buggy and complex but it works well enough.

--- NatureSurvives/Assets/ResourceSpawner.cs
+++ NatureSurvives/Assets/ResourceSpawner.cs
@@ -16,10 +16,14 @@ public class ResourceSpawner : MonoBehaviour
     public int m_iResourceCount;
     [Tooltip("The resource - Enter the following values linearly and in accordance with m_goResources")]
     public GameObject[] m_goResources;
-    [Tooltip("The rarity of the resource")]
+    [Tooltip("The total amount of spawns per resource count NOTE: Spawn Quantity w/UsingDensity == true = Spawn Quantity * Density Quantity")]
     public int[] m_iSpawnQuantity;
     [Tooltip("How far apart the resource(s) should spawn")]
     public int[] m_iSpawnDensity;
+    [Tooltip("The quantity of objects to spawn within the current densiy level")]
+    public int[] m_iDensityQuantity;
+    [Tooltip("Whether or not you're using the de")]
+    public bool m_bUsingDensity; //Could use an array for this and set by object.. cause maybe some objs might just be random and not density defined
     private GameObject m_goGround;
 
 
@@ -58,7 +62,8 @@ public class ResourceSpawner : MonoBehaviour
         m_goResourcesSpawned = new List<GameObject>();
     }
 
-    private Vector3 CreateNewPosition()
+
+    private Vector3 CreateNewPosition(bool _bUsingDensity ,Vector3 _bNearByPos) // have to double check bool because of firstspawn..
     {
         float _iRandXRange;
 
@@ -66,17 +71,39 @@ public class ResourceSpawner : MonoBehaviour
 
         Vector3 _newPos;
         
-
-        for (int i = 0; i < 30; i++)
+        switch(_bUsingDensity)
         {
-            _iRandXRange = Random.Range(m_fGroundRegion[0] * -1.0f, m_fGroundRegion[0]);
-            _iRandZRange = Random.Range(m_fGroundRegion[1] * -1.0f, m_fGroundRegion[1]);
-            _newPos = new Vector3(_iRandXRange, 0.31f, _iRandZRange);
-            if (CheckObjectDistance(_newPos, GameObject.FindGameObjectWithTag("HomeBuilding").transform.position) > 70)
-            {
-                return _newPos;
-            }
+            case false:
+                {
+                    for (int i = 0; i < 30; i++)
+                    {
+                        _iRandXRange = Random.Range(m_fGroundRegion[0] * -1.0f, m_fGroundRegion[0]);
+                        _iRandZRange = Random.Range(m_fGroundRegion[1] * -1.0f, m_fGroundRegion[1]);
+                        _newPos = new Vector3(_iRandXRange, 0.31f, _iRandZRange);
+                        if (CheckObjectDistance(_newPos, GameObject.FindGameObjectWithTag("HomeBuilding").transform.position) > 70)
+                        {
+                            return _newPos;
+                        }
+                    }
+                    break;
+                }
+            case true:
+                {
+                    for (int i = 0; i < 30; i++)
+                    {
+                        _iRandXRange = Random.Range(_bNearByPos.x - 7.5f, _bNearByPos.x - 7.5f); // 15 is default, the range is 15
+                        _iRandZRange = Random.Range(_bNearByPos.z - 7.5f, _bNearByPos.z - 7.5f);
+                        _newPos = new Vector3(_iRandXRange, 0.31f, _iRandZRange);
+                        if (CheckObjectDistance(_newPos, GameObject.FindGameObjectWithTag("HomeBuilding").transform.position) > 70)
+                        {
+                            return _newPos;
+                        }
+                    }
+                    break;
+                }
         }
+
+        // this disregards density - could do if else lol
         _iRandXRange = Random.Range(m_fGroundRegion[0] * -1.0f, m_fGroundRegion[0]);
         _iRandZRange = Random.Range(m_fGroundRegion[1] * -1.0f, m_fGroundRegion[1]);
         _newPos = new Vector3(_iRandXRange, 0.31f, _iRandZRange); // Give Up
@@ -86,34 +113,119 @@ public class ResourceSpawner : MonoBehaviour
      
     }
 
-
-
-    private void SpawnResources()
+    /// <summary>
+    /// pretty shit atm just checks the lists initialized within the start mehtod - trees and bushes... - have a separate check for house in createnewpos
+    /// </summary>
+    /// <returns></returns>
+    private bool CompleteRegionalObjectCheck(GameObject _goObj, bool _bUsingDensity, Vector3 _vec3FirstPos)
     {
-        bool _bSpawned = false;
-        for (int i = 0; i < m_iResourceCount; i++)
+        bool _bSpawned;
+        // Check the newly spawned NPC's position for overlapping
+        switch(_bUsingDensity)
         {
-            for (int j = 0; j < m_iSpawnQuantity[i]; j++)
-            {
-                Vector3 spawnPos = CreateNewPosition();
-                m_goResourcesSpawned.Add(Instantiate(m_goResources[i], spawnPos, Quaternion.identity) as GameObject);
-                _bSpawned = true;
-                // Check the newly spawned NPC's position for overlapping
-                for (int k = 0; k < m_iRegionalObjectCount; k++)
+            case false:
                 {
-                    if (1 < CheckObjectDistance(m_goResourcesSpawned[i].transform.position, m_goRegionalObjects[i].transform.position))
+                    for (int k = 0; k < m_iRegionalObjectCount; k++)
                     {
-                        _bSpawned = GenerateNewPosition(100, m_goResourcesSpawned[i]);
-                        if (!_bSpawned)
+                        if (1 < CheckObjectDistance(_goObj.transform.position, m_goRegionalObjects[k].transform.position))
                         {
-                            print("A new position couldn't be found for the object being spawned");
+                            _bSpawned = GenerateNewPosition(100, _goObj, false, new Vector3(0,0,0)); // 100 attempts
                         }
-
                     }
+                    break;
                 }
-            }
+            case true:
+                {
+                    for (int k = 0; k < m_iRegionalObjectCount; k++)
+                    {
+                        if (1 < CheckObjectDistance(_goObj.transform.position, m_goRegionalObjects[k].transform.position))
+                        {
+                            _bSpawned = GenerateNewPosition(100, _goObj, true, _vec3FirstPos); // 100 attempts
+                        }
+                    }
+                    break;
+                }
         }
-        m_bResourcesSpwaned = true;
+        return true;
+    }
+
+
+
+    private bool SpawnResources()
+    {
+        bool _bSpawned;
+        Vector3 spawnPos;
+        switch (m_bUsingDensity)
+        {
+            case false:
+                {
+                    for (int i = 0; i < m_iResourceCount; i++)
+                    {
+                        for (int j = 0; j < m_iSpawnQuantity[i]; j++)
+                        {
+                            _bSpawned = false;
+                            spawnPos = CreateNewPosition(false,new Vector3(0,0,0));
+                            m_goResourcesSpawned.Add(Instantiate(m_goResources[i], spawnPos, Quaternion.identity) as GameObject);
+                            _bSpawned = CompleteRegionalObjectCheck(m_goResourcesSpawned[j].gameObject, false, new Vector3(0,0,0));
+                            if (!_bSpawned)
+                            {
+                                print("Object had trouble spawning");
+                            }
+                            // Check the newly spawned NPC's position for overlapping
+                        }
+                    }
+                    break;
+                }
+            case true:
+                {
+                    bool _firstSpawn;
+                    Vector3 _vec3FirstSpawnPos = new Vector3();
+                    int _iObjID = -1; // ezier to keep up with because we end up with having to keep track of j * k and k+1 to check objs
+                    for (int i = 0; i < m_iResourceCount; i++)
+                    {
+                        for (int j = 0; j < m_iSpawnDensity[i]; j++)
+                        {
+                            _firstSpawn = true;
+                            for (int k = 0; k < m_iSpawnQuantity[i]; k++)
+                            {
+                                _iObjID++;
+                                if (_firstSpawn)
+                                {
+                                    //createnewpos
+                                    _bSpawned = false;
+                                    spawnPos = CreateNewPosition(false, new Vector3(0, 0, 0));
+                                    m_goResourcesSpawned.Add(Instantiate(m_goResources[i], spawnPos, Quaternion.identity) as GameObject); // COULD JUST CHECK THEIR POS BEFORE SPAWN BUT CEEBS
+                                    _bSpawned = CompleteRegionalObjectCheck(m_goResourcesSpawned[k].gameObject, false, new Vector3(0,0,0));
+                                    _vec3FirstSpawnPos = new Vector3(m_goResourcesSpawned[k].transform.position.x, m_goResourcesSpawned[k].transform.position.y, m_goResourcesSpawned[k].transform.position.z);
+                                    
+                                    _firstSpawn = false;
+                                    print("First Spawn No: " + k);
+                                }
+                                else
+                                {
+                                     for (int l = 0; l < m_iDensityQuantity[i]; l++)
+                                    {
+                                        _bSpawned = false;
+                                        spawnPos = CreateNewPosition(true, _vec3FirstSpawnPos);
+                                        m_goResourcesSpawned.Add(Instantiate(m_goResources[i], spawnPos, Quaternion.identity) as GameObject); // COULD JUST CHECK THEIR POS BEFORE SPAWN BUT CEEBS
+                                        _bSpawned = CompleteRegionalObjectCheck(m_goResourcesSpawned[k+l].gameObject, true, _vec3FirstSpawnPos);
+
+                                        // objid = (i * j + k ())
+                                        _iObjID = ((j * 10) + k); 
+
+
+                                        print("First Spawn No: " + k + " Nearby Object No: " + l);
+                                    }
+                                }
+
+                            }
+                                
+                        }
+                    }
+                    break;
+                }
+        }
+        return true;
     }
 
     private float CheckObjectDistance(Vector3 _objectA, Vector3 _objectB)
@@ -133,15 +245,22 @@ public class ResourceSpawner : MonoBehaviour
         return false;
     }
 
-    private bool GenerateNewPosition(int _iAttempts, GameObject _goObjPosToAlter)
+    private bool GenerateNewPosition(int _iAttempts, GameObject _goObjPosToAlter, bool _bCreatingNearByPos, Vector3 _vec3FirstPos)
     {
             for (int i = 0; i < _iAttempts; i++)
             {
                 bool _bObjClear = false;
 
-
-
-                Vector3 spawnPos = CreateNewPosition();
+                Vector3 spawnPos;
+                if (!_bCreatingNearByPos)
+                {
+                    spawnPos = CreateNewPosition(false, new Vector3(0, 0, 0));
+                }
+                else
+                {
+                // need to set this up more efficiently
+                    spawnPos = CreateNewPosition(true, _vec3FirstPos);
+                }
 
                  _bObjClear = CheckForNearbyObjects(spawnPos);
 
@@ -165,7 +284,7 @@ public class ResourceSpawner : MonoBehaviour
             // havent included scuffedtrees or the corrupt zone for potential hazards to spawns..
             // could do this at build to save doing it say each time we respawn resources.. idk
             print("|| Spawning Resources ||");
-            SpawnResources();
+            m_bResourcesSpwaned = SpawnResources();
         }
 
         // could do some checking here to see when a resources needs refreshing..
