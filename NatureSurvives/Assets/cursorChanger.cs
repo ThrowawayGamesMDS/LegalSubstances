252ae063016503125c2558e816c0289854353a0b
blair <blaircorban@hotmail.com>
2019-03-28 12:32:22 +1300

    added cursor changing
    add the tag of the object that should change the cursor in the tags list and the corresponding cursor in the same place in the other list

---
+++ NatureSurvives/Assets/cursorChanger.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class cursorChanger : MonoBehaviour
+{
+
+    public List<string> CursorTags;
+    public List<Texture2D> AvailableCursors;
+    public CursorMode cursorMode = CursorMode.Auto;
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit _rh;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if(Physics.Raycast(Camera.main.transform.position, ray.direction, out _rh, 1000))
+        {
+            Cursor.SetCursor(AvailableCursors[0], Vector2.zero, cursorMode);
+            for (int i = 0; i < CursorTags.Count; i++)
+            {
+                if(_rh.transform.tag == CursorTags[i])
+                {
+                    Cursor.SetCursor(AvailableCursors[i], Vector2.zero, cursorMode);
+                }
+            }
+        }
+    }
+}
