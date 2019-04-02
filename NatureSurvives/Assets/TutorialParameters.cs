165132023ebee39907b58115d5ad37be90aa1947
BlairCorban <blaircorban@hotmail.com>
2019-04-02 13:11:47 +1300

    updated some of tutorial level

--- NatureSurvives/Assets/TutorialParameters.cs
+++ NatureSurvives/Assets/TutorialParameters.cs
@@ -8,7 +8,7 @@ public class TutorialParameters : MonoBehaviour
     {
         E_ONE = 1,
         E_TWO = 2,
-        E_THIRD = 3
+        E_THREE = 3
     }
     public TutorialState m_eTutorialLevel;
     // Start is called before the first frame update
@@ -20,6 +20,47 @@ public class TutorialParameters : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        switch(m_eTutorialLevel)
+        {
+            //checking if the player has selected all 3 wongles
+            case TutorialState.E_ONE:
+                {
+                    int amountSelected = 0;
+                    GameObject[] temp = GameObject.FindGameObjectsWithTag("Wongle");
+                    for (int i = 0; i < temp.Length; i++)
+                    {
+                        if(temp[i].GetComponent<SelectableUnitComponent>().isSelected)
+                        {
+                            amountSelected++;
+                        }
+                    }
+                    if(amountSelected >= 3)
+                    {
+                        //change ui to completed version (maybe tick boxes)
+                        m_eTutorialLevel = TutorialState.E_TWO;
+                    }
+                    break;
+                }
+            case TutorialState.E_TWO:
+                {
+                    if(HouseController.WhiteAmount >= 200 && HouseController.WoodAmount >= 250 && HouseController.CrystalAmount >= 250)
+                    {
+                        //spawn enemy
+                        //tick ui box
+                        m_eTutorialLevel = TutorialState.E_THREE;
+                    }
+                    break;
+                }
+            case TutorialState.E_THREE:
+                {
+                    //if enemy == null
+                    //show some "you finished tutorial" garbage and then send them to main level
+                    break;
+                }
+            default:
+                {
+                    break;
+                }
+        }
     }
 }
