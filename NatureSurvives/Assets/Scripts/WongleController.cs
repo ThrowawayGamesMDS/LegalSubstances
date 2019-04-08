e4bac67a8a0040b984a46e7801e113ee5b6e3f72
Rimanah <benjllewellynmobile@gmail.com>
2019-04-08 12:44:40 +1200

    Revert "Merge branch 'master' of https://github.com/ThrowawayGamesMDS/LegalSubstances"
    This reverts commit 199d467d1510eb2ae254fcc57bd72f6b630ee392, reversing
    changes made to ab0594fa0fd8f6ee4047f1413c61931bd110790b.

--- NatureSurvives/Assets/Scripts/WongleController.cs
+++ NatureSurvives/Assets/Scripts/WongleController.cs
@@ -125,7 +125,7 @@ public class WongleController : MonoBehaviour
         iFishingLevel = Mathf.FloorToInt(Mathf.Sqrt((iFishCaught / 3)));
         iOverallLevel = iWoodCutLevel + iMineLevel + iFarmLevel;
 
-        agent.speed = 10 + iOverallLevel;
+        agent.speed = 7 + iOverallLevel;
 
 
 
